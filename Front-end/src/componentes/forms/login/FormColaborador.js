dc528ae61d19c07d5bf91f49d2a5ed907265be78
kvojps <joseferreira.santosjunior@upe.br>
2022-05-12 18:55:37 -0300

    adicionando useState no formulario de login do colaborador

--- Front-end/src/componentes/forms/login/FormColaborador.js
+++ Front-end/src/componentes/forms/login/FormColaborador.js
@@ -1,20 +1,47 @@
-import styles from '../assets/Form.module.css'
+import styles from "../assets/Form.module.css";
+import { useState } from "react";
 
 function FormColaborador() {
-    return(
+  const [form, setForm] = useState({
+    email: "",
+    senha: "",
+    tipo: "colaborador",
+  });
+
+  return (
+    <div>
+      <div className={styles.form}>
+        <h1>Entre como colaborador</h1>
+        <p>Digite seu e-mail</p>
+        <input
+          type="email"
+          placeholder="Ex.: propague@gmail.com"
+          onChange={(e) =>
+            setForm({
+              ...form,
+              email: e.target.value,
+            })
+          }
+        />
+        <p>Digite sua senha</p>
+        <input
+          type="password"
+          name=""
+          id=""
+          placeholder="Senha"
+          onChange={(e) =>
+            setForm({
+              ...form,
+              senha: e.target.value,
+            })
+          }
+        />
         <div>
-        <div className={styles.form}>
-            <h1>Entre como colaborador</h1>
-            <p>Digite seu e-mail</p>
-            <input type="email" placeholder="Ex.: propague@gmail.com"/>
-            <p>Digite sua senha</p>
-            <input type="password" placeholder="Senha" />
-            <div>
-                <button>Entrar como colaborador</button>
-            </div>
+          <button>Entrar como colaborador</button>
         </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default FormColaborador;
