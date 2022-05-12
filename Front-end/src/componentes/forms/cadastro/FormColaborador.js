79cbbbc321e6d4c538920e0aa0ba75d8a0425144
kvojps <joseferreira.santosjunior@upe.br>
2022-05-12 18:33:03 -0300

    adicionando usestate ao formulariode cadastro do colaborador

--- Front-end/src/componentes/forms/cadastro/FormColaborador.js
+++ Front-end/src/componentes/forms/cadastro/FormColaborador.js
@@ -1,22 +1,60 @@
-import styles from '../assets/Form.module.css'
+import styles from "../assets/Form.module.css";
+import {useState} from "react"
 
 function Colaborador() {
-    return(
+  const [form, setForm] = useState({
+    email: "",
+    senha: "",
+    senhaRepetida: "",
+    tipo: "colaborador",
+  });
+
+  return (
+    <div>
+      <div className={styles.form}>
+        <h1>Seja um colaborador!</h1>
+        <input
+          type="email"
+          placeholder="Ex.: propague@gmail.com"
+          onChange={(e) =>
+            setForm({
+              ...form,
+              email: e.target.value,
+            })
+          }
+        />
+        <p>Digite sua senha</p>
+        <input
+          type="password"
+          name=""
+          id=""
+          placeholder="Senha"
+          onChange={(e) =>
+            setForm({
+              ...form,
+              senha: e.target.value,
+            })
+          }
+        />
+        <p>Repita sua senha</p>
+        <input
+          type="password"
+          name=""
+          id=""
+          placeholder="Senha"
+          onChange={(e) =>
+            setForm({
+              ...form,
+              senhaRepetida: e.target.value,
+            })
+          }
+        />
         <div>
-        <div className={styles.form}>
-            <h1>Seja um colaborador!</h1>
-            <p>Digite seu e-mail</p>
-            <input type="email" placeholder="Ex.: propague@gmail.com"/>
-            <p>Digite sua senha</p>
-            <input type="password" placeholder="Senha" />
-            <p>Repita sua senha</p>
-            <input type="password" placeholder="Senha" />
-            <div>
-                <button>Cadastre-se</button>
-            </div>
+          <button>Cadastre-se</button>
         </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Colaborador
+export default Colaborador;
