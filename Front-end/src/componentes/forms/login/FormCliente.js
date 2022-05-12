57bad54b857a756c6824ddeee21f5de26bcf8a7b
kvojps <joseferreira.santosjunior@upe.br>
2022-05-12 18:48:57 -0300

    adicionando useState no formulario de login do cliente

--- Front-end/src/componentes/forms/login/FormCliente.js
+++ Front-end/src/componentes/forms/login/FormCliente.js
@@ -1,18 +1,44 @@
-import styles from '../assets/Form.module.css';
-
+import styles from "../assets/Form.module.css";
+import { useState } from "react";
 function FormCliente() {
-    return(
-        <div className={styles.form}>
-            <h1>Entre como cliente</h1>
-            <p>Digite seu e-mail</p>
-            <input type="email" placeholder="Ex.: propague@gmail.com"/>
-            <p>Digite sua senha</p>
-            <input type="password" name="" id="" placeholder="Senha" />
-            <div>
-                <button>Entrar como cliente</button>
-            </div>
-        </div>
-    )
+  const [form, setForm] = useState({
+    email: "",
+    senha: "",
+    tipo: "cliente",
+  });
+
+  return (
+    <div className={styles.form}>
+      <h1>Entre como cliente</h1>
+      <p>Digite seu e-mail</p>
+      <input
+        type="email"
+        placeholder="Ex.: propague@gmail.com"
+        onChange={(e) =>
+          setForm({
+            ...form,
+            email: e.target.value,
+          })
+        }
+      />
+      <p>Digite sua senha</p>
+      <input
+        type="password"
+        name=""
+        id=""
+        placeholder="Senha"
+        onChange={(e) => 
+            setForm({
+                ...form,
+                senha: e.target.value,
+            })
+        }
+      />
+      <div>
+        <button>Entrar como cliente</button>
+      </div>
+    </div>
+  );
 }
 
 export default FormCliente;
