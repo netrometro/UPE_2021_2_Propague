edad3ac4e72e98a26e60fdcded3debaabf001050
kvojps <joseferreira.santosjunior@upe.br>
2022-05-12 18:23:38 -0300

    adicionando useState no formulario de cadastro do cliente

--- Front-end/src/componentes/forms/cadastro/FormCliente.js
+++ Front-end/src/componentes/forms/cadastro/FormCliente.js
@@ -1,20 +1,59 @@
-import styles from '../assets/Form.module.css'
+import styles from "../assets/Form.module.css";
+import { useState } from "react";
 
 function Cliente() {
-    return(
-        <div className={styles.form}>
-            <h1>Seja um cliente!</h1>
-            <p>Digite seu e-mail</p>
-            <input type="email" placeholder="Ex.: propague@gmail.com"/>
-            <p>Digite sua senha</p>
-            <input type="password" name="" id="" placeholder="Senha" />
-            <p>Repita sua senha</p>
-            <input type="password" name="" id="" placeholder="Senha" />
-            <div>
-                <button>Cadastre-se</button>
-            </div>
-        </div>
-    )
+  const [form, setForm] = useState({
+    email: "",
+    senha: "",
+    senhaRepetida: "",
+    tipo: "cliente",
+  });
+
+  return (
+    <div className={styles.form}>
+      <h1>Seja um cliente!</h1>
+      <p>Digite seu e-mail</p>
+      <input
+        type="email"
+        placeholder="Ex.: propague@gmail.com"
+        onChange={(e) =>
+          setForm({
+            ...form,
+            email: e.target.value,
+          })
+        }
+      />
+      <p>Digite sua senha</p>
+      <input
+        type="password"
+        name=""
+        id=""
+        placeholder="Senha"
+        onChange={(e) =>
+          setForm({
+            ...form,
+            senha: e.target.value,
+          })
+        }
+      />
+      <p>Repita sua senha</p>
+      <input
+        type="password"
+        name=""
+        id=""
+        placeholder="Senha"
+        onChange={(e) =>
+          setForm({
+            ...form,
+            senhaRepetida: e.target.value,
+          })
+        }
+      />
+      <div>
+        <button>Cadastre-se</button>
+      </div>
+    </div>
+  );
 }
 
-export default Cliente
+export default Cliente;
