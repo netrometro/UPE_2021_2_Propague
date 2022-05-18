1b612cd203524e8118f89a33bb2f16e14f4e38a3
kvojps <joseferreira.santosjunior@upe.br>
2022-05-18 14:31:20 -0300

    adicionando controllerUsuario

--- Back-end/src/main/java/br/com/propague/api/controller/UsuarioController.java
+++ Back-end/src/main/java/br/com/propague/api/controller/UsuarioController.java
@@ -1,11 +1,52 @@
 package br.com.propague.api.controller;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.propague.api.model.TipoUsuario;
+import br.com.propague.api.model.Usuario;
+import br.com.propague.api.service.UsuarioService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/usuario")
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class UsuarioController {
+	private final UsuarioService servico;
+	
+	@GetMapping("/usuarios")
+	public ResponseEntity<List<Usuario>> getUsuarios() {
+		return ResponseEntity.ok().body(servico.getUsuarios());
+	}
+	
+	@PostMapping("/usuario/salvar")
+	public ResponseEntity<Usuario> salvarUsuario(@RequestBody Usuario usuario) {
+		URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+		return ResponseEntity.created(uri).body(servico.salvarUsuario(usuario));
+	}
+	
+	@PostMapping("/tipousuario/salvar")
+	public ResponseEntity<TipoUsuario> salvarTipoUsuario(@RequestBody TipoUsuario tipoUsuario) {
+		URI uri = URI.create(
+				ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/tipoUsuario/salvar").toUriString());
+		return ResponseEntity.created(uri).body(servico.salvarTipoUsuario(tipoUsuario));
+	}
+	
+	@PostMapping("/tipousuario/adicionaraousuario")
+	public ResponseEntity<?> addTipoParaUsuario(TipoParaUsuarioForm form) {
+		servico.atribuirTipoAoUsuario(form.getEmail(), form.getTipoUsuarioNome());
+		return ResponseEntity.ok().build();
+	}
+	
 
 //    @Autowired
 //    private IUsuarioService iUsuarioService;
@@ -35,4 +76,10 @@ public class UsuarioController {
 //        iUsuarioService.deleteById(id);
 //        return ResponseEntity.noContent().build();
 //    }
+}
+
+@Data
+class TipoParaUsuarioForm {
+	private String email;
+	private String tipoUsuarioNome;
 }
