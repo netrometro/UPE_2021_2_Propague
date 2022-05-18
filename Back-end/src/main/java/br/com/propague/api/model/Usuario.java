8a3bf012e92d417fd26cf858b044eda88ecb73b6
kvojps <joseferreira.santosjunior@upe.br>
2022-05-18 13:47:12 -0300

    refatorando entidade usuário e adicionando entidade TipoUsuario

--- Back-end/src/main/java/br/com/propague/api/model/Usuario.java
+++ Back-end/src/main/java/br/com/propague/api/model/Usuario.java
@@ -1,19 +1,29 @@
 package br.com.propague.api.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 import javax.persistence.*;
 
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     private String nome;
-
     private String email;
-
     private String senha;
+    
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Collection<TipoUsuario> tipos = new ArrayList<>();
+    
+    
 }
