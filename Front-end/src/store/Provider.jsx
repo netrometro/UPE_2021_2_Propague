3396b477b09f06c19e5a38cb613a53f262a0b3e8
Gabriel Lima <gabriel.rtlima@upe.br>
2022-05-19 17:07:21 -0300

    criando Provider

---
+++ Front-end/src/store/Provider.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Context from './Context';
+import useStorage from '../utils/useStorage';
+
+const Provider = ({ children }) => {
+    const [token, setToken] = useStorage('token');
+
+    return(
+        <Context.Provider
+            value={{
+                token,
+                setToken
+            }}
+        >
+            { children }
+        </Context.Provider>
+    );
+}
+
+export default Provider;
