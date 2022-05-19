9a5640b75c51ccc0212024f095bf6ac8c4373a36
Gabriel Lima <gabriel.rtlima@upe.br>
2022-05-19 17:09:08 -0300

    configurando rotas privadas

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -14,22 +14,29 @@ import Navbar from './componentes/layouts/Navbar'
 import Container from './componentes/layouts/Container'
 import Propaganda from './componentes/paginas/Propaganda';
 
+import Provider from './store/Provider'
+import PrivateRoute from './routes/private/Private';
+
 function App() {
   return (
     <div>
       <Router>
         <Navbar/>
-        <Container>
-          <Routes>
-            <Route exact path="/" element={<Home />}></Route>
-            <Route path="/contato" element={<Contato />}></Route>
-            <Route path="/sobre" element={<Sobre />}></Route>
-            <Route path="/entrar" element={<Entrar />}></Route>
-            <Route path="/cadastro" element={<Cadastro/>}></Route>
-            <Route path="/listagem" element={<ListagemProjetos />}></Route>
-            <Route path="/propaganda" element={<Propaganda/>}></Route>
-          </Routes>
-        </Container>
+        <Provider>
+          <Container>
+            <Routes>
+              <Route exact path="/" element={<Home />}></Route>
+              <Route path="/contato" element={<Contato />}></Route>
+              <Route path="/sobre" element={<Sobre />}></Route>
+              <Route path="/entrar" element={<Entrar />}></Route>
+              <Route path="/cadastro" element={<Cadastro/>}></Route>
+              <Route exact path='/' element={<PrivateRoute/>}>
+                <Route path="/listagem" element={<ListagemProjetos />}></Route>
+                <Route path="/propaganda" element={<Propaganda/>}></Route>
+              </Route>
+            </Routes>
+          </Container>
+        </Provider>
       </Router>
     </div>
   );
