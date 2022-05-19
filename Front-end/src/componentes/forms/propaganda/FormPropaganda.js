125a2de3763ef588a009fac064e792335f1cf70a
Emilio <emiliogabriel000@gmail.com>
2022-05-19 10:04:44 -0300

    Adição do UseState para salvar dados de publicidade no FormPropaganda

--- Front-end/src/componentes/forms/propaganda/FormPropaganda.js
+++ Front-end/src/componentes/forms/propaganda/FormPropaganda.js
@@ -1,6 +1,15 @@
 import styles from "../assets/Form.module.css"
+import {useState} from "react"
+
 
 function Propaganda() {
+  const[form, setForm] = useState({
+    empresa: "",
+    contato: "",  
+    midia: "",
+    proprietario: "",
+    publicidade: "",
+  })
 
     return(
         <div className={styles.form}>
@@ -8,26 +17,66 @@ function Propaganda() {
       <input
         type="text"
         placeholder="Ex.: Propague"
+        name=""
+        id=""
+        onChange={(e) =>
+          setForm({
+            ...form,
+            empresa: e.target.value,
+          })
+        }
       />
       <p>Endereço de contato</p>
       <input
         type="text"
         placeholder="Ex.: (87) 991920341"
+        name=""
+        id=""
+        onChange={(e) =>
+          setForm({
+            ...form,
+            contato: e.target.value,
+          })
+        }
       />
       <p>Veiculação da mídia</p>
       <input
         type="text"
         placeholder="Ex.: Instagram"
+        name=""
+        id=""
+        onChange={(e) =>
+          setForm({
+            ...form,
+            midia: e.target.value,
+          })
+        }
       />
       <p>Nome do proprietário</p>
       <input
         type="text"
         placeholder="Ex.: Carlos"
+        name=""
+        id=""
+        onChange={(e) =>
+          setForm({
+            ...form,
+            proprietario: e.target.value,
+          })
+        }
       />
       <p>Motivo de sua publicidade</p>
       <input
         type="text"
         placeholder="Ex.: Promoção de fim de ano!"
+        name=""
+        id=""
+        onChange={(e) =>
+          setForm({
+            ...form,
+            publicidade: e.target.value,
+          })
+        }
       />
       <div>
         <button>Publicar</button>
