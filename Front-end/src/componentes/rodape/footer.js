d7979c698f6b2a8d49501ad416d8fed2e7eb7292
Emilio <emiliogabriel000@gmail.com>
2022-05-21 23:11:18 -0300

    Rodape com navegação

--- Front-end/src/componentes/rodape/footer.js
+++ Front-end/src/componentes/rodape/footer.js
@@ -1,23 +1,38 @@
 import React from "react";
 import styles from "../rodape/Rodape.module.css"
+
+
+
 const Footer = () => {
     return(
-        <div className={styles.Box}>
-            <h1 style={{ color: "white", 
-                   textAlign: "center", 
-                   marginTop: "60px" }}>Propague: Faça suas propagandas!</h1>
+        <div className={styles.footer}>
             <div className={styles.Container}>
             <div className={styles.Row}>
             <div className={styles.Column}>
-            <div className={styles.Heading}>Teste</div>
-                <div className={styles.FooterLink} href="#">Aim</div>
+            <div className={styles.Heading}>Contato</div>
+            <div className={styles.margin}>
+            <a className={styles.link} href="/faleconosco">fale conosco</a></div>
+            </div>
+
+            <div className={styles.Column}>
+            <div className={styles.Heading}>Sobre</div>
+            <div className={styles.margin}>
+            <a className={styles.link} href="/sobre">sobre nós</a>
+            </div>
             </div>
             <div className={styles.Column}>
-            <div className={styles.Heading}>Teste2</div>
-                <div className={styles.FooterLink} href="#">Aim</div>
+            <div className={styles.Heading}>Entrar</div>
+            <div className={styles.margin}  >
+            <a className={styles.link} href="/entrar">faça login</a>
+            </div>
             </div>
 
-
+            <div className={styles.Column}>
+            <div className={styles.Heading}>Cadastrar</div>
+            <div className={styles.margin}>
+            <a className={styles.link} href="/cadastro">se cadastre</a>
+            </div>
+            </div>
             </div>
             </div>
 
