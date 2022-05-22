c3c4db135af29fb92f48084cb183a01bc3a75221
kvojps <joseferreira.santosjunior@upe.br>
2022-05-22 19:05:35 -0300

    adicionando repositorio de midia

---
+++ Back-end/src/main/java/br/com/propague/api/repository/MidiaRepository.java
@@ -0,0 +1,8 @@
+package br.com.propague.api.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.propague.api.model.Midia;
+
+public interface MidiaRepository extends JpaRepository<Midia, Long> {
+
+}
