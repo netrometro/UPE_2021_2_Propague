aebe4b8192056698bd33c0b265678acafe80fcec
Gabriel Lima <gabriel.rtlima@upe.br>
2022-05-22 22:13:59 -0300

    adicionando refresh token ao controller

--- Back-end/src/main/java/br/com/propague/api/controller/UsuarioController.java
+++ Back-end/src/main/java/br/com/propague/api/controller/UsuarioController.java
@@ -1,9 +1,20 @@
 package br.com.propague.api.controller;
 
+import java.io.IOException;
 import java.net.URI;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +27,11 @@ import br.com.propague.api.model.Usuario;
 import br.com.propague.api.service.UsuarioService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/api")
@@ -45,6 +61,44 @@ public class UsuarioController {
 	public ResponseEntity<?> addTipoParaUsuario(TipoParaUsuarioForm form) {
 		servico.atribuirTipoAoUsuario(form.getEmail(), form.getTipoUsuarioNome());
 		return ResponseEntity.ok().build();
+	}
+
+	@GetMapping("/token/refresh")
+	public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String authorizationHeader = request.getHeader("Authorization");
+		if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+			try{
+				String refresh_token = authorizationHeader.substring(7);
+				Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+				JWTVerifier verifier = JWT.require(algorithm).build();
+				DecodedJWT decodedJWT = verifier.verify(refresh_token);
+				String username = decodedJWT.getSubject();
+				Usuario usuario = servico.getUsuario(username);
+				String acces_token = com.auth0.jwt.JWT.create()
+						.withSubject(usuario.getEmail())
+						.withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+						.withIssuer(request.getRequestURL().toString())
+						.withClaim("tipo", usuario.getTipos().stream().map(TipoUsuario::getNome).collect(Collectors.joining()))
+						.sign(algorithm);
+//        response.setHeader("acces_token", token);
+//        response.setHeader("refresh_token", refresh_token);
+				Map<String, String> map = new HashMap<>();
+				map.put("token", acces_token);
+				map.put("refresh_token", refresh_token);
+				response.setContentType(APPLICATION_JSON_VALUE);
+				new ObjectMapper().writeValue(response.getOutputStream(), map);
+			} catch(Exception e) {
+				response.setHeader("error", e.getMessage());
+				response.setStatus(403);
+				Map<String, String> map = new HashMap<>();
+				map.put("error", e.getMessage());
+				response.setContentType(MimeTypeUtils.APPLICATION_JSON_VALUE);
+				new ObjectMapper().writeValue(response.getOutputStream(), map);
+			}
+
+		}else {
+			throw new RuntimeException("Refresh token is missing");
+		}
 	}
 	
 
