dcfe63d99a721e16890e880f7bf837dabb85811e
Gabriel Lima <gabriel.rtlima@upe.br>
2022-05-22 22:11:14 -0300

    criando configuração de cors

---
+++ Back-end/src/main/java/br/com/propague/api/security/CorsConfiguration.java
@@ -0,0 +1,22 @@
+package br.com.propague.api.security;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
