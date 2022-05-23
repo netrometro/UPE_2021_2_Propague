83b024fb5bc7973833f94247c187179dd0ab6bc5
Gabriel Lima <gabriel.rtlima@upe.br>
2022-05-22 22:14:51 -0300

    comunicando service com o security

--- Back-end/src/main/java/br/com/propague/api/service/UsuarioService.java
+++ Back-end/src/main/java/br/com/propague/api/service/UsuarioService.java
@@ -6,25 +6,38 @@ import br.com.propague.api.repository.TipoUsuarioRepository;
 import br.com.propague.api.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class UsuarioService implements IUsuarioService {
+@Slf4j
+public class UsuarioService implements IUsuarioService, UserDetailsService {
 	private final UsuarioRepository repositorioUsuario;
 	private final TipoUsuarioRepository repositorioTipoUsuario;
+	private final PasswordEncoder passwordEncoder;
 
 	@Override
 	public Usuario salvarUsuario(Usuario usuario) {
-		// É necessário a parte de segurança configurada
-		return null;
+		log.info("Salvando usuário: " + usuario.getEmail());
+		usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
+		return repositorioUsuario.save(usuario);
 	}
 
 	@Override
@@ -47,6 +60,27 @@ public class UsuarioService implements IUsuarioService {
 	@Override
 	public List<Usuario> getUsuarios() {
 		return repositorioUsuario.findAll();
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Usuario usuario = repositorioUsuario.findByEmail(username);
+		if(usuario == null) {
+			log.error("Usuário não encontrado: " + username);
+			throw new UsernameNotFoundException("Usuário não encontrado");
+		} else {
+			log.info("Usuário encontrado: " + username);
+		}
+		Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+		usuario.getTipos().forEach(tipoUsuario -> authorities.add(new SimpleGrantedAuthority(tipoUsuario.getNome())));
+		return new org.springframework.security.core.userdetails.User(usuario.getEmail(), usuario.getSenha(), authorities);
+
+	}
+
+	public Collection<? extends GrantedAuthority> getAuthorities(Usuario usuario) {
+		return usuario.getTipos().stream()
+				.map(tipoUsuario -> new SimpleGrantedAuthority(tipoUsuario.getNome()))
+				.collect(Collectors.toList());
 	}
 
 //    @Autowired
