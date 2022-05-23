e0888029efcadb875de4857fe0f4105ee3b7bb22
Gabriel Lima <gabriel.rtlima@upe.br>
2022-05-22 22:12:45 -0300

    criando bean de passwordEncoder

--- Back-end/src/main/java/br/com/propague/api/ApiApplication.java
+++ Back-end/src/main/java/br/com/propague/api/ApiApplication.java
@@ -2,7 +2,9 @@ package br.com.propague.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableJpaRepositories(basePackages = {"br.com.propague.api"})
 @SpringBootApplication
@@ -12,4 +14,8 @@ public class ApiApplication {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
