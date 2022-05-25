a98cb215ab5a2ba875d3c7030d2c7ad205f014ac
Emilio <emiliogabriel000@gmail.com>
2022-05-25 08:11:24 -0300

    Estruturação do hamburger icon - Erro AddEventListener

--- Front-end/src/componentes/layouts/Navbar.js
+++ Front-end/src/componentes/layouts/Navbar.js
@@ -3,15 +3,27 @@ import styles from './assets/Navbar.module.css';
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+
+    const Hamburger = document.getElementsByClassName('hamburger')[0]
+    const List = document.getElementsByClassName('list')[0]
+    Hamburger.addEventListener('click', () => {
+        List.classList.toggle('active')
+    })
+
     return (
         <nav className={styles.navbar}>
             <Link to="/" className={styles.slogan}>
                 <p>Propague</p>
             </Link>
+            <a href="#" className={styles.hamburger}>
+                <span className={styles.bar}></span>
+                <span className={styles.bar}></span>
+                <span className={styles.bar}></span>
+            </a>
             <ul className={styles.list}>
-            <li className={styles.navItem}><Link to="/faleconosco">Fale Conosco</Link></li>
-            <li className={styles.navItem}><Link to="/sobre">Sobre</Link></li>
-            <li className={styles.navItem}><Link to="/entrar">Entrar</Link></li>
+            <li><Link to="/faleconosco">Fale Conosco</Link></li>
+            <li><Link to="/sobre">Sobre</Link></li>
+            <li><Link to="/entrar">Entrar</Link></li>
             </ul>
         </nav>
     )
