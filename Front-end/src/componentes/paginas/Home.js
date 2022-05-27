f52d9d0df782b5aae667e77755ac5fe3e92b4609
kvojps <joseferreira.santosjunior@upe.br>
2022-05-27 14:08:40 -0300

    adicionando imagem a pagina home

--- Front-end/src/componentes/paginas/Home.js
+++ Front-end/src/componentes/paginas/Home.js
@@ -1,5 +1,6 @@
 import styles from './assets/Home.module.css';
 import LinkButton from '../layouts/LinkButton'
+import img from '../../img/img-home.webp'
 
 function Home() {
     return(
@@ -7,6 +8,7 @@ function Home() {
             <h1>Bem vindo ao <span>Propague</span></h1>
             <p>Anuncie seu produto e veja os anúncios de outras pessoas</p>
             <LinkButton to="/propaganda" text="Criar anúncio"/>
+            <img src={img} alt="Propague" />
         </div>
     )
 }
