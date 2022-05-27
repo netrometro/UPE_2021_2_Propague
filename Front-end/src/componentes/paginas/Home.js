bca22d74864f049a9ce2d73c415ee53bd2eca807
kvojps <joseferreira.santosjunior@upe.br>
2022-05-27 14:01:28 -0300

    adicionando linkButton a pagina home

--- Front-end/src/componentes/paginas/Home.js
+++ Front-end/src/componentes/paginas/Home.js
@@ -1,10 +1,12 @@
 import styles from './assets/Home.module.css';
+import LinkButton from '../layouts/LinkButton'
 
 function Home() {
     return(
         <div>
             <h1>Bem vindo ao <span>Propague</span></h1>
             <p>Anuncie seu produto e veja os anúncios de outras pessoas</p>
+            <LinkButton to="/propaganda" text="Criar anúncio"/>
         </div>
     )
 }
