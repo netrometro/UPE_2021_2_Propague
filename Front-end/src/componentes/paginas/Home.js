8394d4b2ac5969cf6c10d0b74f4a32ee973f4dd9
kvojps <joseferreira.santosjunior@upe.br>
2022-05-27 13:51:58 -0300

    criando conteudo da pagina home

--- Front-end/src/componentes/paginas/Home.js
+++ Front-end/src/componentes/paginas/Home.js
@@ -3,7 +3,8 @@ import styles from './assets/Home.module.css';
 function Home() {
     return(
         <div>
-            <h1>Home</h1>
+            <h1>Bem vindo ao <span>Propague</span></h1>
+            <p>Anuncie seu produto e veja os anúncios de outras pessoas</p>
         </div>
     )
 }
